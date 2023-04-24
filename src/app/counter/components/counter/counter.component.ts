950539f2feecbe214ea833e542b8b2cefa6677e2
Julian Arias <drowkex2200arise@gmail.com>
2023-04-25 01:59:10 +0200

    primer commit

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector:  'app-counter',
+  template: `
+  <h3>Counter: {{ counter }}</h3>
+  <button (click)="increaseBY(1)">+1</button>
+  <button (click)="increaseBY(-1)">-1</button>
+  <button (click)="reset()">Reset</button>`
+})
+
+export class CounterComponent  {
+
+  public counter: number = 10;
+
+  constructor() { }
+  increaseBY(num: number):void
+  {
+   this.counter+=num;
+  }
+
+  reset():void
+  {
+   this.counter= 10;
+  }
+
+
+}
